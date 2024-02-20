00498ea47a53fc9c732471f2e91b9e5ac61d0a7c
Sebikostudio <sebikostudio@gmail.com>
2024-02-20 18:10:00 +0800

    Update heading

--- src/src/Heading.js
+++ src/src/Heading.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './Heading.scss'; // Assuming you have a corresponding stylesheet
+import React from "react";
+import "./Heading.scss"; // Assuming you have a corresponding stylesheet
 
-const Heading = ({ level = 1, weight = 'regular', children }) => {
+const Heading = ({ level = 1, weight = "regular", style, children }) => {
   const HeadingTag = `h${level}`;
   const weightClass = `heading-${weight}`;
 
   return (
-    <HeadingTag className={weightClass}>
+    <HeadingTag className={weightClass} style={style}>
       {children}
     </HeadingTag>
   );
